084a69491887b529b437b891423cdaaf8911663f
Jeewana Rasidu <jeewana.rasidu1999@gmail.com>
2022-09-22 14:21:57 +0530

    second change

--- new.py
+++ new.py
@@ -1,4 +1,10 @@
-name = input("Enter you name: ")
-year = int(input("Enter your birth year: "))
-age = 2022 - year
-print("Age: "+str(age))
+##name = input("Enter you name: ")
+##year = int(input("Enter your birth year: "))
+##age = 2022 - year
+##print("Age: "+str(age))
+
+print("Enter two numbers")
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+total = num1 + num2
+print("Total: "+str(total))
